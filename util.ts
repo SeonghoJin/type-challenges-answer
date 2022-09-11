01ed9ff22957c0cf3ffd8d99a1ed0e43cb1c7600
moretall <seongho4996@naver.com>
2022-09-12 00:05:56 +0900

    Update util.ts

--- util.ts
+++ util.ts
@@ -1,2 +1,5 @@
 type FirstChar<T> = T extends `${infer A}${infer B}` ? A: '';
 type RestString<T> = T extends `${infer A}${infer B}` ? B: '';
+
+type FirstElement<T extends Array<any>> = T extends [infer First, ...infer Rest] ? First : [];
+type RestArray<T extends Array<any>> = T extends [infer First, ...infer Rest] ? Rest: [];
